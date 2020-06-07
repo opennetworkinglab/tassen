34b3e13ea0358822a0d88c60f319c6375b688520
Carmelo Cascone <carmelo@opennetworking.org>
2020-06-06 18:09:43 -0700

    Store action profile groups and members

--- mapr/translate/p4rt_store.go
+++ mapr/translate/p4rt_store.go
@@ -22,15 +22,43 @@ type P4RtStore interface {
 	TableEntries() []*p4v1.TableEntry
 	// Returns the number of table entries in the store.
 	TableEntryCount() int
+	// Stores the given action profile group.
+	PutActProfGroup(*p4v1.ActionProfileGroup)
+	// Returns the action profile group associated with the given key, or nil.
+	GetActProfGroup(*string) *p4v1.ActionProfileGroup
+	// Removes the given action profile group.
+	RemoveActProfGroup(*p4v1.ActionProfileGroup)
+	// Returns a slice of action profile groups that satisfy the predicate f.
+	FilterActProfGroups(f func(*p4v1.ActionProfileGroup) bool) []*p4v1.ActionProfileGroup
+	// Returns all action profile groups.
+	ActProfGroups() []*p4v1.ActionProfileGroup
+	// Returns the number of action profile groups in the store.
+	ActProfGroupCount() int
+	// Stores the given action profile member.
+	PutActProfMember(*p4v1.ActionProfileMember)
+	// Returns the action profile member associated with the given key, or nil.
+	GetActProfMember(*string) *p4v1.ActionProfileMember
+	// Removes the given action profile member.
+	RemoveActProfMember(*p4v1.ActionProfileMember)
+	// Returns a slice of action profile members that satisfy the predicate f.
+	FilterActProfMembers(f func(*p4v1.ActionProfileMember) bool) []*p4v1.ActionProfileMember
+	// Returns all action profile members.
+	ActProfMembers() []*p4v1.ActionProfileMember
+	// Returns the number of action profile members in the store.
+	ActProfMemberCount() int
 }
 
 type p4RtStore struct {
-	tableEntries map[string]*p4v1.TableEntry
+	tableEntries   map[string]*p4v1.TableEntry
+	actProfGroups  map[string]*p4v1.ActionProfileGroup
+	actProfMembers map[string]*p4v1.ActionProfileMember
 }
 
 func NewP4RtStore() *p4RtStore {
 	return &p4RtStore{
-		tableEntries: make(map[string]*p4v1.TableEntry),
+		tableEntries:   make(map[string]*p4v1.TableEntry),
+		actProfGroups:  make(map[string]*p4v1.ActionProfileGroup),
+		actProfMembers: make(map[string]*p4v1.ActionProfileMember),
 	}
 }
 
@@ -43,6 +71,18 @@ func (s *p4RtStore) Update(req *p4v1.WriteRequest, dryRun bool) error {
 				s.RemoveTableEntry(x.TableEntry)
 			} else {
 				s.PutTableEntry(x.TableEntry)
+			}
+		case *p4v1.Entity_ActionProfileGroup:
+			if u.Type == p4v1.Update_DELETE {
+				s.RemoveActProfGroup(x.ActionProfileGroup)
+			} else {
+				s.PutActProfGroup(x.ActionProfileGroup)
+			}
+		case *p4v1.Entity_ActionProfileMember:
+			if u.Type == p4v1.Update_DELETE {
+				s.RemoveActProfMember(x.ActionProfileMember)
+			} else {
+				s.PutActProfMember(x.ActionProfileMember)
 			}
 		default:
 			log.WithField("WriteRequest", req).Warnf("Storing %T not implemented, ignoring...", x)
@@ -94,4 +134,86 @@ func (s *p4RtStore) TableEntries() []*p4v1.TableEntry {
 
 func (s *p4RtStore) TableEntryCount() int {
 	return len(s.tableEntries)
+}
+
+func ActProfGroupKey(actProfId uint32, groupIp uint32) string {
+	return fmt.Sprintf("%d-%d", actProfId, groupIp)
+}
+
+// Returns a string that uniquely identifies the given table entry.
+func KeyFromActProfGroup(g *p4v1.ActionProfileGroup) string {
+	return ActProfGroupKey(g.ActionProfileId, g.GroupId)
+}
+
+func (s *p4RtStore) PutActProfGroup(g *p4v1.ActionProfileGroup) {
+	s.actProfGroups[KeyFromActProfGroup(g)] = g
+}
+
+func (s *p4RtStore) GetActProfGroup(key *string) *p4v1.ActionProfileGroup {
+	return s.actProfGroups[*key]
+}
+
+func (s *p4RtStore) RemoveActProfGroup(g *p4v1.ActionProfileGroup) {
+	delete(s.actProfGroups, KeyFromActProfGroup(g))
+}
+
+func (s *p4RtStore) FilterActProfGroups(f func(*p4v1.ActionProfileGroup) bool) []*p4v1.ActionProfileGroup {
+	filtered := make([]*p4v1.ActionProfileGroup, 0)
+	for _, value := range s.actProfGroups {
+		if f(value) {
+			filtered = append(filtered, value)
+		}
+	}
+	return filtered
+}
+
+func (s *p4RtStore) ActProfGroups() []*p4v1.ActionProfileGroup {
+	return s.FilterActProfGroups(func(*p4v1.ActionProfileGroup) bool {
+		return true
+	})
+}
+
+func (s *p4RtStore) ActProfGroupCount() int {
+	return len(s.actProfGroups)
+}
+
+func ActProfMemberKey(actProfId uint32, memberId uint32) string {
+	return fmt.Sprintf("%d-%d", actProfId, memberId)
+}
+
+// Returns a string that uniquely identifies the given table entry.
+func KeyFromActProfMember(g *p4v1.ActionProfileMember) string {
+	return ActProfMemberKey(g.ActionProfileId, g.MemberId)
+}
+
+func (s *p4RtStore) PutActProfMember(g *p4v1.ActionProfileMember) {
+	s.actProfMembers[KeyFromActProfMember(g)] = g
+}
+
+func (s *p4RtStore) GetActProfMember(key *string) *p4v1.ActionProfileMember {
+	return s.actProfMembers[*key]
+}
+
+func (s *p4RtStore) RemoveActProfMember(g *p4v1.ActionProfileMember) {
+	delete(s.actProfMembers, KeyFromActProfMember(g))
+}
+
+func (s *p4RtStore) FilterActProfMembers(f func(*p4v1.ActionProfileMember) bool) []*p4v1.ActionProfileMember {
+	filtered := make([]*p4v1.ActionProfileMember, 0)
+	for _, value := range s.actProfMembers {
+		if f(value) {
+			filtered = append(filtered, value)
+		}
+	}
+	return filtered
+}
+
+func (s *p4RtStore) ActProfMembers() []*p4v1.ActionProfileMember {
+	return s.FilterActProfMembers(func(*p4v1.ActionProfileMember) bool {
+		return true
+	})
+}
+
+func (s *p4RtStore) ActProfMemberCount() int {
+	return len(s.actProfMembers)
 }
